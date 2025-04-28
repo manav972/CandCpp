278e3547b95090d7689c0028adff9a6ee9dc45c7
rupalpatel <rupalpatel2783@gmail.com>
2025-04-28 13:40:12 +0530

    c.6.1

---
+++ PR. 1 Multi-Utility Program/c 6.1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+	float c, fh;
+	printf("enter temperature in centigrade\n");
+	scanf("%f", &c);
+	fh = (c*9/5)+32;
+	printf("temperature in fahrenheitis %f\n", fh);
+	}
