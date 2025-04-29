959ab91aceeb70466fbe29a29918197bda60fd85
rupalpatel <rupalpatel2783@gmail.com>
2025-04-29 13:35:19 +0530

    ternary operator

---
+++ ternary operator/c. 1.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+main(){
+	int a,b,c;
+	printf("enter value of a=");
+	scanf("%d",&a);
+	printf("enter value of b=");
+	scanf("%d",&b);
+	printf("enter value of c=");
+	scanf("%d",&c);
+	if (a>b&&a<c){
+	
+	printf("a is max");
+}
+else if (b>a && b>c){
+
+printf("b is max");
+}
+
+else if(c>a&&c>b){
+
+printf("c is max");
+}else{
+	printf("all are equal");
+	}
+
+	if(a==b && b==c && c==a){
+	
+	printf("all are equal");
+}else{
+	printf("c is max");
+}
+	}
+
+	
+
